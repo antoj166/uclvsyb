644591bebb06f031ae9bbb6cd7bb94c528ba7f79
Abolanosup <104039035+Abolanosup@users.noreply.github.com>
2022-06-18 14:18:41 -0400

    Update conf.py

--- conf.py
+++ conf.py
@@ -1,3 +1,3 @@
 api_hash = "9d8097d05bbc90a6ed2a7a81abcd4e8a"
 api_id = 9902519
-bot_token = "5484895015:AAGigjr2WhuVxaSfBEFYJ5LOAD1d1AOcz7g"
+bot_token = "5457176222:AAF09rSPQ8YdmNnjGwMcpsPgl15sEa9zmsI"
